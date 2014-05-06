b9de51d33887328a7f32e1821db9bbdadaccd43c
rwhig-1NS <big.higgi@gmail.com>
2014-05-06 20:07:44 -0300

    added static text to testdata.php for testing

--- application/views/testdata.php
+++ application/views/testdata.php
@@ -1,3 +1,10 @@
-<?php 
-
-printr($users);
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>testdata</title>
+</head>
+<body>
+	<?php echo "Hello, World!!"; ?>
+</body>
+</html>
