50268a65a3be40af31052563790a5c964e95cf51
Moontasir <acenovember@gmail.com>
2014-05-08 23:11:22 -0300

    test

--- application/views/templates/main_footer.php
+++ application/views/templates/main_footer.php
@@ -19,45 +19,10 @@
 	</div><!-- /.modal -->
 
 
-
-	  
 	        
 	        <script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 
-	        <script type='text/javascript' src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-
-
-	        <!-- JavaScript jQuery code from Bootply.com editor -->
-	        
-	        <script type='text/javascript'>
-	        
-	        $(document).ready(function() {
-	        
-	            $(".alert").addClass("in").fadeOut(4500);
-
-	/* swap open/close side menu icons */
-	$('[data-toggle=collapse]').click(function(){
-	  	// toggle icon
-	  	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
-	});
-				
-				
-				$('#myTab a').click(function (e) {
-					e.preventDefault()
-					$(this).tab('show')
-				})
-				
-				$(function () {
-					$('#myTab a:last').tab('show')
-				})
-
-	        
-	        </script>
-			
-			
-			
-		
-	        
+	        <script type='text/javascript' src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>	        
     </body>
 </html>
