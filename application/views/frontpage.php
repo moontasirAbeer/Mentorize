2306e9bbe320bb72e074985f975aae7ab370d7f3
rwhig-1NS <big.higgi@gmail.com>
2014-05-10 05:52:23 -0300

    .

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,4 +1,9 @@
-<?php if( session_status() == 'PHP_SESSION_NONE') {session_start();} ?>
+<?php 
+$status = session_status();
+if($status  == 'PHP_SESSION_NONE') {
+		session_start();
+	} 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
